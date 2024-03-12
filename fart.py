7e5f720f89bc42f8ed5197187d1ad4b32d8caa5e
Stitch <coolguygameplayer52@gmail.com>
2024-03-12 05:28:33 -0400

    why
    hey did you know that discord added tracking parameters to urls now? and that images won't load unless you have those parameters?
    
    anyway this works again

--- fart.py
+++ fart.py
@@ -3,4 +3,4 @@ import os
 i=0
 while i>=-10:
     i += 1
-    os.system(f"curl https://cdn.discordapp.com/attachments/964179289757081630/967194225152241754/1628711303-vlc.png -o {i}.png")
+    os.system(f"curl 'https://cdn.discordapp.com/attachments/422440665486721035/1217041363841978469/1628711303-vlc.png?ex=66029518&is=65f02018&hm=27b63f3c65841d585ef217afc2f569c5189b3b219984d0fb0e52f5d66a536a62&' -o {i}.png")
