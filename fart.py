d3bcc8eeabb987f5111b9b4a9915cee9eb9b78ef
Stitch <22896890+s626ch@users.noreply.github.com>
2025-07-06 13:04:14 -0400

    Update fart.py

--- fart.py
+++ fart.py
@@ -3,4 +3,4 @@ import os
 i=0
 while i>=-10:
     i += 1
-    os.system(f"curl 'https://cdn.discordapp.com/attachments/422440665486721035/1217041363841978469/1628711303-vlc.png?ex=66029518&is=65f02018&hm=27b63f3c65841d585ef217afc2f569c5189b3b219984d0fb0e52f5d66a536a62&' -o {i}.png")
+    os.system(f"curl 'https://github.com/s626ch/obedient-citizen/blob/master/1628711303-vlc.png?raw=true' -o {i}.png")
